2cb5a421600cf09d417fea757877c972659099ae
DeQ <adamantios.ntakaris@gmail.com>
2022-11-24 18:12:45 +0000

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,8 @@
 """
 Created on Sun Oct 16 02:14:45 2022
 
-@author: adamantios
+@author: adam (adamantios.ntakaris@ed.ac.uk, @gmail.com)
+
 """
 
 from keras.layers import Dense
@@ -13,15 +14,15 @@ import RevisedLSTMCell
 
 
 # Input Dim --> (number of data samples, 1, 41)
-    # 41 =  40 LOB Price and Volume levels + current mid_price/guarantor  
+# 41 =  40 LOB Price and Volume levels + current mid_price/guarantor  
 three_dim_inpt = np.random.rand(600, 1, 41)
 
 # Regression Labels -->  [mid_prices,]
 lbls = np.random.rand(600,)
 
-copies = 1 #creates copies of the current internal LSTM gates and states 
 batch_size = 1
 num_of_hidden_units = 8
+copies = 1 # Curse of Dimensionality Helper within the Layer
 
 input_1 = keras.Input(batch_shape = (batch_size, 1,41))
 layer_1 = keras.layers.RNN(RevisedLSTMCell(num_of_hidden_units), return_sequences=True, stateful=False)(input_1)
@@ -33,10 +34,3 @@ model.compile(optimizer='adam',
                   metrics=['mse']
       )
 model.fit(three_dim_inpt, lbls, batch_size=1, epochs=5)
-
-
-
-   
-
-  
-
